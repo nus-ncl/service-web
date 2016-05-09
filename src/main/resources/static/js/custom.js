d2cb2ca9a1de359f51aff666713dbc95cb8bced1
teye <a0097953@gmail.com>
2016-05-09 18:03:16 +0800

    add simple progress bar to sign up page (#DEV-136)

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
         $('.current').removeClass('current').hide()
             .next().show().addClass('current');
             //activate next step on progressbar using the index of next_fs
-            $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+            // $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
     });
 
     $('.btn-prev').click(function() {
@@ -26,7 +26,7 @@ $(document).ready(function() {
         $('.current').removeClass('current').hide()
             .prev().show().addClass('current');
             //de-activate current step on progressbar
-            $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+            // $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
     });
     
     $('.apply-create-team').click(function() {
