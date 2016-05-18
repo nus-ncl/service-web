acf84dc6f7579fe35e2480aeb3118ba203c32647
teye <a0097953@gmail.com>
2016-05-18 17:58:39 +0800

    fix news and events again (#DEV-214)

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -6,6 +6,38 @@
 
 $(document).ready(function() {
 	
+	/***************** Waypoints ******************/
+	$('.wp1').waypoint(function() {
+		$('.wp1').addClass('animated fadeInLeft');
+	}, {
+		offset: '75%'
+	});
+	$('.wp2').waypoint(function() {
+		$('.wp2').addClass('animated fadeInUp');
+	}, {
+		offset: '75%'
+	});
+	$('.wp3').waypoint(function() {
+		$('.wp3').addClass('animated fadeInDown');
+	}, {
+		offset: '55%'
+	});
+	$('.wp4').waypoint(function() {
+		$('.wp4').addClass('animated fadeInDown');
+	}, {
+		offset: '75%'
+	});
+	$('.wp5').waypoint(function() {
+		$('.wp5').addClass('animated fadeInUp');
+	}, {
+		offset: '75%'
+	});
+	$('.wp6').waypoint(function() {
+		$('.wp6').addClass('animated fadeInDown');
+	}, {
+		offset: '75%'
+	});
+	
 	/* Register Form */
 	/* Hide all except first fieldset */
     $('.btn-next').click(function() {
@@ -242,4 +274,43 @@ jQuery(function($) {
 	}
 	google.maps.event.addDomListener(window, 'load', initialize_map);
 	
+});
+
+/***************** Flexsliders ******************/
+
+$(window).load(function() {
+
+	$('#portfolioSlider').flexslider({
+		animation: "slide",
+		directionNav: false,
+		controlNav: true,
+		touch: false,
+		pauseOnHover: true,
+		start: function() {
+			$.waypoints('refresh');
+		}
+	});
+
+	$('#servicesSlider').flexslider({
+		animation: "slide",
+		directionNav: false,
+		controlNav: true,
+		touch: true,
+		pauseOnHover: true,
+		start: function() {
+			$.waypoints('refresh');
+		}
+	});
+
+	$('#teamSlider').flexslider({
+		animation: "slide",
+		directionNav: false,
+		controlNav: true,
+		touch: true,
+		pauseOnHover: true,
+		start: function() {
+			$.waypoints('refresh');
+		}
+	});
+
 });
