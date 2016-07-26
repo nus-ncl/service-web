de8641454cefd197d0c5f933421df44a41ca60a6
teye <a0097953@gmail.com>
2016-07-26 15:35:57 +0800

    add team page apply team form tests

---
+++ src/test/java/sg/ncl/testbed_interface/TeamPageApplyTeamFormTest.java
@@ -0,0 +1,97 @@
+package sg.ncl.testbed_interface;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * @author Te Ye
+ */
+public class TeamPageApplyTeamFormTest {
+
+    @Test
+    public void testGetTeamName() {
+        final TeamPageApplyTeamForm one = new TeamPageApplyTeamForm();
+        assertThat(one.getTeamName(), is(nullValue()));
+    }
+
+    @Test
+    public void testSetLoginEmail() {
+        final TeamPageApplyTeamForm one = new TeamPageApplyTeamForm();
+        final String str = RandomStringUtils.randomAlphanumeric(20);
+        one.setTeamName(str);
+        assertThat(one.getTeamName(), is(str));
+    }
+
+    @Test
+    public void testGetTeamDescription() {
+        final TeamPageApplyTeamForm one = new TeamPageApplyTeamForm();
+        assertThat(one.getTeamDescription(), is(nullValue()));
+    }
+
+    @Test
+    public void testSetTeamDescription() {
+        final TeamPageApplyTeamForm one = new TeamPageApplyTeamForm();
+        final String str = RandomStringUtils.randomAlphanumeric(20);
+        one.setTeamDescription(str);
+        assertThat(one.getTeamDescription(), is(str));
+    }
+
+    @Test
+    public void testGetTeamWebsite() {
+        final TeamPageApplyTeamForm one = new TeamPageApplyTeamForm();
+        assertThat(one.getTeamDescription(), is(nullValue()));
+    }
+
+    @Test
+    public void testSetTeamWebsite() {
+        final TeamPageApplyTeamForm one = new TeamPageApplyTeamForm();
+        final String str = RandomStringUtils.randomAlphanumeric(20);
+        one.setTeamWebsite(str);
+        assertThat(one.getTeamWebsite(), is(str));
+    }
+
+    @Test
+    public void testGetTeamOrganizationType() {
+        final TeamPageApplyTeamForm one = new TeamPageApplyTeamForm();
+        assertThat(one.getTeamOrganizationType(), is(nullValue()));
+    }
+
+    @Test
+    public void testSetTeamOrganizationType() {
+        final TeamPageApplyTeamForm one = new TeamPageApplyTeamForm();
+        final String str = RandomStringUtils.randomAlphanumeric(20);
+        one.setTeamOrganizationType(str);
+        assertThat(one.getTeamOrganizationType(), is(str));
+    }
+
+    @Test
+    public void testGetTeamIsPublic() {
+        final TeamPageApplyTeamForm one = new TeamPageApplyTeamForm();
+        assertThat(one.getIsPublic(), is(TeamVisibility.PUBLIC.toString()));
+    }
+
+    @Test
+    public void testSetTeamIsPublic() {
+        final TeamPageApplyTeamForm one = new TeamPageApplyTeamForm();
+        one.setIsPublic(TeamVisibility.PRIVATE.toString());
+        assertThat(one.getIsPublic(), is(TeamVisibility.PRIVATE.toString()));
+    }
+
+    @Test
+    public void testGetTeamHasAcceptTeamOwnerPolicy() {
+        final TeamPageApplyTeamForm one = new TeamPageApplyTeamForm();
+        assertThat(one.getHasAcceptTeamOwnerPolicy(), is(false));
+    }
+
+    @Test
+    public void testSetTeamHasAcceptTeamOwnerPolicy() {
+        final TeamPageApplyTeamForm one = new TeamPageApplyTeamForm();
+        one.setHasAcceptTeamOwnerPolicy(true);
+        assertThat(one.getHasAcceptTeamOwnerPolicy(), is(true));
+    }
+
+}
