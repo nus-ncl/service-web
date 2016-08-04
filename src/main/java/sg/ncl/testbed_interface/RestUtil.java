89e1e539be99320ceb5b6ca4066c9bb3a6ab6eb2
teye <a0097953@gmail.com>
2016-08-04 14:52:16 +0800

    create custom rest template error handler

---
+++ src/main/java/sg/ncl/testbed_interface/RestUtil.java
@@ -0,0 +1,15 @@
+package sg.ncl.testbed_interface;
+
+/**
+ * @author Te Ye
+ */
+import org.springframework.http.HttpStatus;
+
+public class RestUtil {
+
+    public static boolean isError(HttpStatus status) {
+        HttpStatus.Series series = status.series();
+        return (HttpStatus.Series.CLIENT_ERROR.equals(series)
+                || HttpStatus.Series.SERVER_ERROR.equals(series));
+    }
+}
