6b3f641cbbc8473ce5738d491abfe697fe2524e8
teye <a0097953@gmail.com>
2016-08-15 15:00:56 +0800

    add exception states

--- src/main/java/sg/ncl/MainController.java
+++ src/main/java/sg/ncl/MainController.java
@@ -469,6 +469,9 @@ public class MainController {
                 } catch (TeamNotFoundException e) {
                     redirectAttributes.addFlashAttribute("message", e.getMessage());
                     return "redirect:/signup2";
+                } catch (ApplyNewProjectException e) {
+                    redirectAttributes.addFlashAttribute("message", e.getMessage());
+                    return "redirect:/signup2";
                 } catch (Exception e) {
                     redirectAttributes.addFlashAttribute("message", ERR_SERVER_OVERLOAD);
                     return "redirect:/signup2";
@@ -506,6 +509,12 @@ public class MainController {
             } catch (AdapterConnectionException e) {
                 redirectAttributes.addFlashAttribute("message", e.getMessage());
                 return "redirect:/signup2";
+            } catch (ApplyNewProjectException e) {
+                redirectAttributes.addFlashAttribute("message", e.getMessage());
+                return "redirect:/signup2";
+            } catch (Exception e) {
+                redirectAttributes.addFlashAttribute("message", ERR_SERVER_OVERLOAD);
+                return "redirect:/signup2";
             }
 
             logger.info("Signup join team success");
@@ -524,7 +533,7 @@ public class MainController {
      * Use when registering new accounts
      * @param mainObject A JSONObject that contains user's credentials, personal details and team application details
      */
-    private void registerUserToDeter(JSONObject mainObject) throws WebServiceRuntimeException, TeamNotFoundException, AdapterConnectionException {
+    private void registerUserToDeter(JSONObject mainObject) throws WebServiceRuntimeException, TeamNotFoundException, AdapterConnectionException, ApplyNewProjectException {
         HttpEntity<String> request = createHttpEntityWithBody(mainObject.toString());
         restTemplate.setErrorHandler(new MyResponseErrorHandler());
         ResponseEntity response = restTemplate.exchange(properties.getSioRegUrl(), HttpMethod.POST, request, String.class);
@@ -538,6 +547,9 @@ public class MainController {
                 if (error.getName().equals(ExceptionState.JoinProjectException.toString())) {
                     logger.warn("Register new users join team request : team name error");
                     throw new TeamNotFoundException("Team name does not exists");
+                } else if (error.getName().equals(ExceptionState.ApplyNewProjectException.toString())) {
+                    logger.warn("Register new users new team request : team name error");
+                    throw new ApplyNewProjectException();
                 } else {
                     logger.warn("Registration or adapter connection fail");
                     // possible sio or adapter connection fail
