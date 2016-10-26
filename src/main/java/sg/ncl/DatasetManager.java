7d4b3a9318fcdb0b6ac0a3bf6ae68a749cd49627
James Ng <jng@ncl.sg>
2016-10-26 16:47:58 +0800

    DEV-717 Removed redundant sample init methods

--- src/main/java/sg/ncl/DatasetManager.java
+++ src/main/java/sg/ncl/DatasetManager.java
@@ -13,63 +13,6 @@ public class DatasetManager {
 		datasetMap = new HashMap<Integer, Dataset>();
 	}
 
-	public void initSamples() {
-	    String ACCESSIBILITY_OPEN = "open";
-        String ACCESSIBILITY_RESTRICTED = "restricted";
-        String VISIBILITY_PUBLIC = "public";
-        String VISIBILITY_PRIVATE = "private";
-
-		Dataset dataset1 = new Dataset();
-		dataset1.setId(generateRandomId());
-		dataset1.setName("DataAries");
-        dataset1.setDescription("this is a simple desc");
-		dataset1.setContributorId("206");
-		dataset1.setVisibility(VISIBILITY_PUBLIC);
-		dataset1.setAccessibility(ACCESSIBILITY_OPEN);
-
-		Dataset dataset2 = new Dataset();
-        dataset2.setId(generateRandomId());
-		dataset2.setName("DataTaurus");
-        dataset2.setDescription("this is a simple desc");
-        dataset2.setContributorId("206");
-		dataset2.setVisibility(VISIBILITY_PRIVATE);
-		dataset2.setAccessibility(ACCESSIBILITY_OPEN);
-
-		Dataset dataset3 = new Dataset();
-        dataset3.setId(generateRandomId());
-		dataset3.setName("DataGemini");
-        dataset3.setDescription("this is a simple desc");
-		dataset3.setContributorId("206");
-		dataset3.setVisibility(VISIBILITY_PUBLIC);
-		dataset3.setAccessibility(ACCESSIBILITY_RESTRICTED);
-
-		Dataset dataset4 = new Dataset();
-        dataset4.setId(generateRandomId());
-		dataset4.setName("DataCancer");
-        dataset4.setDescription("this is a simple desc");
-		dataset4.setContributorId("200");
-		dataset4.setVisibility(VISIBILITY_PUBLIC);
-        dataset4.setAccessibility(ACCESSIBILITY_RESTRICTED);
-
-		datasetMap.put(dataset1.getId(), dataset1);
-		datasetMap.put(dataset2.getId(), dataset2);
-		datasetMap.put(dataset3.getId(), dataset3);
-		datasetMap.put(dataset4.getId(), dataset4);
-    }
-    
-	private int generateRandomId() {
-    	Random rn = new Random();
-    	int datasetId = rn.nextInt(Integer.MAX_VALUE) + 1;
-    	while (isIdExists(datasetId)) {
-    		datasetId = rn.nextInt();
-    	}
-    	return datasetId;
-    }
-	
-	private boolean isIdExists(int id) {
-		return datasetMap.containsKey(id);
-	}
-	
 	public boolean removeDataset(int id) {
 		if (datasetMap.containsKey(id)) {
 			datasetMap.remove(id);
