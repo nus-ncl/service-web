cc750edbdcfdfbcb77855b4088c4cd9ff4503675
teye <a0097953@gmail.com>
2016-10-29 22:01:55 +0800

    DEV-759 add image map

--- src/main/java/sg/ncl/testbed_interface/Image.java
+++ src/main/java/sg/ncl/testbed_interface/Image.java
@@ -1,10 +1,14 @@
 package sg.ncl.testbed_interface;
 
+import lombok.Getter;
+import lombok.Setter;
 import sg.ncl.domain.ImageVisibility;
 
 /**
  * Created by dcsyeoty on 29-Oct-16.
  */
+@Getter
+@Setter
 public class Image {
 
     private String id;
