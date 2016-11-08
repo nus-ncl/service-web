7a71029f548a3bae9abbb1640046d31902a7d12e
teye <a0097953@gmail.com>
2016-11-08 10:48:05 +0800

    DEV-789 make session variables invalidate on error

--- src/main/java/sg/ncl/AppConfig.java
+++ src/main/java/sg/ncl/AppConfig.java
@@ -1,21 +1,28 @@
 package sg.ncl;
 
 import nz.net.ultraq.thymeleaf.LayoutDialect;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.inject.Inject;
 
 /**
  * @author by Te Ye
  */
 @Configuration("sg.ncl.AppConfig")
 public class AppConfig {
-    @Autowired
+
+    @Inject
     private ErrorAttributes errorAttributes;
 
+    @Inject
+    private WebProperties webProperties;
+
     @Bean
-    public AppErrorController appErrorController(){return new AppErrorController(errorAttributes);}
+    public AppErrorController appErrorController() {
+        return new AppErrorController(errorAttributes, webProperties);
+    }
 
     // thymleaf-layout-dialect
     // for fragments
