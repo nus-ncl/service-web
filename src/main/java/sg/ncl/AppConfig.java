eac6fb7535a108625ef1b917656bbc1a8da1f624
teye <a0097953@gmail.com>
2016-11-09 12:50:43 +0800

    DEV-792 fix error attributes

--- src/main/java/sg/ncl/AppConfig.java
+++ src/main/java/sg/ncl/AppConfig.java
@@ -1,10 +1,12 @@
 package sg.ncl;
 
 import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.inject.Inject;
 import javax.validation.constraints.NotNull;
 
 /**
@@ -13,8 +15,14 @@ import javax.validation.constraints.NotNull;
 @Configuration("sg.ncl.AppConfig")
 public class AppConfig {
 
+    @Autowired
+    private ErrorAttributes errorAttributes;
+
+    @Inject
+    private WebProperties webProperties;
+
     @Bean
-    public AppErrorController appErrorController(@NotNull ErrorAttributes errorAttributes, @NotNull WebProperties webProperties) {
+    public AppErrorController appErrorController() {
         return new AppErrorController(errorAttributes, webProperties);
     }
 
