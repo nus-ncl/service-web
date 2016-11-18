6dfb5176e0077dd1a339b0a9d644d76aec6e8c88
teye <a0097953@gmail.com>
2016-11-18 12:20:23 +0800

    DEV-816 add checks for password reset form

--- src/main/java/sg/ncl/testbed_interface/PasswordResetForm.java
+++ src/main/java/sg/ncl/testbed_interface/PasswordResetForm.java
@@ -16,6 +16,8 @@ public class PasswordResetForm {
     private String errMsg;
 
     public boolean isPasswordOk() {
+        // order of check determines which error message to display first
+
         if(!this.getPassword1().equals(this.getPassword2())) {
             this.setErrMsg("Password not match!");
             return false;
@@ -28,6 +30,16 @@ public class PasswordResetForm {
 
         if(this.getPassword1().contains(" ")) {
             this.setErrMsg("Password cannot contain whitespace!");
+            return false;
+        }
+
+        if (!this.getPassword1().matches("(?=.*[0-9]).+")) {
+            this.setErrMsg("Password must contain at least 1 digit.");
+            return false;
+        }
+
+        if (!this.getPassword1().matches("(?=.*[a-zA-Z]).+")) {
+            this.setErrMsg("Password must contain at least 1 alphabet.");
             return false;
         }
 
