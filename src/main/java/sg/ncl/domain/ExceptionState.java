b00bced21d7cff678d7d11cfdb9be303681d8e75
teye <a0097953@gmail.com>
2016-11-22 16:16:04 +0800

    DEV-827 rearrange exception state to alphabetical order

--- src/main/java/sg/ncl/domain/ExceptionState.java
+++ src/main/java/sg/ncl/domain/ExceptionState.java
@@ -8,35 +8,36 @@ import java.util.Map;
 /**
  * @author Te Ye
  */
+// arrange in alphabet order for ease of tracking
 public enum ExceptionState {
-    USER_NOT_FOUND_EXCEPTION("UserNotFoundException"),
-    CREDENTIALS_UPDATE_EXCEPTION("CredentialsUpdateException"),
-    NS_FILE_PARSE_EXCEPTION("NSFileParseException"),
-    EXP_NAME_ALREADY_EXISTS_EXCEPTION("ExpNameAlreadyExistsException"),
-    TEAM_NOT_FOUND_EXCEPTION("TeamNotFoundException"),
+    ADAPTER_DETERLAB_CONNECT_EXCEPTION("AdapterDeterlabConnectException"),
     APPLY_NEW_PROJECT_EXCEPTION("ApplyNewProjectException"),
-    REGISTER_TEAM_NAME_DUPLICATE_EXCEPTION("TeamNameDuplicateException"),
-    JOIN_PROJECT_EXCEPTION("JoinProjectException"),
-    EXP_START_EXCEPTION("ExpStartException"),
-    EXP_DELETE_EXCEPTION("ExpDeleteException"),
-    ID_NULL_OR_EMPTY_EXCEPTION("IdNullOrEmptyException"),
-    INVALID_TEAM_STATUS_EXCEPTION("InvalidTeamStatusException"),
-    EXPERIMENT_NAME_IN_USE_EXCEPTION("ExperimentNameInUseException"),
-    USERNAME_ALREADY_EXISTS_EXCEPTION("UsernameAlreadyExistsException"),
     CREDENTIALS_NOT_FOUND_EXCEPTION("CredentialsNotFoundException"),
-    EMAIL_NOT_VERIFIED_EXCEPTION("EmailNotVerifiedException"),
-    USER_NOT_APPROVED_EXCEPTION("UserNotApprovedException"),
-    INVALID_CREDENTIALS_EXCEPTION("InvalidCredentialsException"),
+    CREDENTIALS_UPDATE_EXCEPTION("CredentialsUpdateException"),
+    DATASET_NAME_IN_USE_EXCEPTION("DatasetNameInUseException"),
     EMAIL_ALREADY_EXISTS_EXCEPTION("EmailAlreadyExistsException"),
+    EMAIL_NOT_VERIFIED_EXCEPTION("EmailNotVerifiedException"),
+    EXP_DELETE_EXCEPTION("ExpDeleteException"),
+    EXP_START_EXCEPTION("ExpStartException"),
+    EXP_NAME_ALREADY_EXISTS_EXCEPTION("ExpNameAlreadyExistsException"),
+    EXPERIMENT_NAME_IN_USE_EXCEPTION("ExperimentNameInUseException"),
     FORBIDDEN_EXCEPTION("ForbiddenException"),
+    ID_NULL_OR_EMPTY_EXCEPTION("IdNullOrEmptyException"),
+    INVALID_CREDENTIALS_EXCEPTION("InvalidCredentialsException"),
+    INVALID_PASSWORD_EXCEPTION("InvalidPasswordException"),
+    INVALID_TEAM_NAME_EXCEPTION("InvalidTeamNameException"),
+    INVALID_TEAM_STATUS_EXCEPTION("InvalidTeamStatusException"),
+    JOIN_PROJECT_EXCEPTION("JoinProjectException"),
+    NS_FILE_PARSE_EXCEPTION("NSFileParseException"),
+    OBJECT_OPTIMISTIC_LOCKING_FAILURE_EXCEPTION("ObjectOptimisticLockingFailureException"),
     PASSWORD_RESET_REQUEST_TIMEOUT_EXCEPTION("PasswordResetRequestTimeoutException"),
     PASSWORD_RESET_REQUEST_NOT_FOUND_EXCEPTION("PasswordResetRequestNotFoundException"),
-    ADAPTER_DETERLAB_CONNECT_EXCEPTION("AdapterDeterlabConnectException"),
+    REGISTER_TEAM_NAME_DUPLICATE_EXCEPTION("TeamNameDuplicateException"),
     TEAM_NAME_ALREADY_EXISTS_EXCEPTION("TeamNameAlreadyExistsException"),
-    INVALID_TEAM_NAME_EXCEPTION("InvalidTeamNameException"),
-    INVALID_PASSWORD_EXCEPTION("InvalidPasswordException"),
-    DATASET_NAME_IN_USE_EXCEPTION("DatasetNameInUseException"),
-    OBJECT_OPTIMISTIC_LOCKING_FAILURE_EXCEPTION("ObjectOptimisticLockingFailureException");
+    TEAM_NOT_FOUND_EXCEPTION("TeamNotFoundException"),
+    USER_NOT_APPROVED_EXCEPTION("UserNotApprovedException"),
+    USER_NOT_FOUND_EXCEPTION("UserNotFoundException"),
+    USERNAME_ALREADY_EXISTS_EXCEPTION("UsernameAlreadyExistsException");
 
     private static final Map<String, ExceptionState> map = new HashMap<>();
 
