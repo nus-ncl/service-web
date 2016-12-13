6c7265f2b22eb7b267c8195d8275db9c26577305
Tran Ly Vu <vutransingapore@gmail.com>
2016-12-13 16:08:16 +0800

    .

--- src/main/java/sg/ncl/domain/ExceptionState.java
+++ src/main/java/sg/ncl/domain/ExceptionState.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @author Te Ye
+ * @author Te Ye, Vu
  */
 public enum ExceptionState {
     ADAPTER_CONNECTION_EXCEPTION("AdapterConnectionException"),
