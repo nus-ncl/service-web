0257a5632895c10ec69bf21e018510512b230c22
teye <a0097953@gmail.com>
2017-01-06 15:39:14 +0800

    DEV-865 remove unused imports

--- src/main/java/sg/ncl/testbed_interface/Team2.java
+++ src/main/java/sg/ncl/testbed_interface/Team2.java
@@ -7,7 +7,6 @@ import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.EnumMap;
-import java.util.HashMap;
 import java.util.List;
 
 /**
