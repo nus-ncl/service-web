c96876c9f5d7e02f934510179bb7995e2c9a29c0
teye <a0097953@gmail.com>
2017-03-24 17:52:18 +0800

    rename html files

--- src/main/java/sg/ncl/MainController.java
+++ src/main/java/sg/ncl/MainController.java
@@ -267,7 +267,7 @@ public class MainController {
     public String testbedNodesStatus(Model model) throws IOException {
         Map<MachineType, List> nodesStatus = getNodesStatus();
         model.addAttribute("nodesStatus", nodesStatus);
-    return "testbednodesstatus";
+    return "testbed_nodes_status";
     }
 
 
