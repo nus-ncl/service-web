99c1b9d437c01847fd258f6869e4b028c39abed3
James Ng <jng@ncl.sg>
2017-03-28 12:47:47 +0800

    Added check for verified user when approving team by admin

--- src/main/java/sg/ncl/MainController.java
+++ src/main/java/sg/ncl/MainController.java
@@ -2615,6 +2615,10 @@ public class MainController {
                             teamOwnerId);
                     redirectAttributes.addFlashAttribute(MESSAGE, "UserId cannot be null or empty");
                     break;
+                case EMAIL_NOT_VERIFIED_EXCEPTION:
+                    log.warn("Approve team: User {} email not verified", teamOwnerId);
+                    redirectAttributes.addFlashAttribute(MESSAGE, "User has not been verified");
+                    break;
                 case INVALID_TEAM_STATUS_EXCEPTION:
                     log.warn("Approve team: TeamStatus is invalid");
                     redirectAttributes.addFlashAttribute(MESSAGE, "Team status is invalid");
