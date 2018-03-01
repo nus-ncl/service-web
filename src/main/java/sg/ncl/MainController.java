d41149d963a1b436346eb858552d4a3c17cfa1ce
James Ng <jng@ncl.sg>
2018-03-01 17:05:02 +0800

    Replace the synchronized class "StringBuffer" by an unsynchronized "StringBuilder"

--- src/main/java/sg/ncl/MainController.java
+++ src/main/java/sg/ncl/MainController.java
@@ -2877,9 +2877,9 @@ public class MainController {
 
     // Reference: http://www.baeldung.com/sha-256-hashing-java
     private String bytesToHex(byte[] hash) {
-        StringBuffer hexString = new StringBuffer();
-        for (int i = 0; i < hash.length; i++) {
-            String hex = Integer.toHexString(0xff & hash[i]);
+        StringBuilder hexString = new StringBuilder();
+        for (byte h : hash) {
+            String hex = Integer.toHexString(0xff & h);
             if (hex.length() == 1) hexString.append('0');
             hexString.append(hex);
         }
