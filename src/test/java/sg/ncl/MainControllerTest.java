7ac8e15067db418eb5c5cf5b9206e60d2e2273f4
Deepshikha121009 <deepshikha121009@gmail.com>
2021-11-24 19:37:09 +0800

    Merging DEV-1245 with DEV-1249

--- src/test/java/sg/ncl/MainControllerTest.java
+++ src/test/java/sg/ncl/MainControllerTest.java
@@ -1,6 +1,7 @@
 package sg.ncl;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Ignore;
@@ -30,6 +31,8 @@ import sg.ncl.testbed_interface.User2;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpSession;
+
+import java.util.StringJoiner;
 
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.Is.is;
@@ -1323,4 +1326,29 @@ public class MainControllerTest {
                 .andExpect(content().string(containsString("/logout")))
                 .andExpect(content().string(containsString("footer id=\"footer\"")));
     }
+
+    @Test
+    public void testMaintainancePage() throws Exception {
+
+        mockMvc.perform(get("/maintainance"))
+                .andExpect(status().isOk())
+                .andExpect(redirectedUrl("/maintainance"));
+    }
+
+
+    @Test
+    public void testWebSSHLogin() throws Exception {
+
+        final String nodeID = "n" + RandomUtils.nextInt(0,9);
+        final String expName = RandomStringUtils.randomAlphabetic(10);
+        final String teamName = RandomStringUtils.randomAlphabetic(10);
+        final StringJoiner qualifiedName = new StringJoiner("&");
+        qualifiedName.add(nodeID).add(expName).add(teamName).add("ncl&sg");
+
+        mockMvc.perform(get("/web_ssh/access_node/{qualifiedName:&+}", qualifiedName))
+                .andExpect(status().isNotFound())
+                .andExpect(redirectedUrl("/maintainance"));
+    }
+
 }
+
