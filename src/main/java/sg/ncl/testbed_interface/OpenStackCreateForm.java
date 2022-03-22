bbf0467d1f78245afb79a33889fd474373d8d9ee
Deepshikha121009 <84137750+Deepshikha121009@users.noreply.github.com>
2022-03-22 23:18:24 +0800

    [DEV-1263] Add experiment scenarios and OS account activation (#347)
    This PR includes these commits:
    * Add a link to experiment to activate openStack and OpenStack create user API call.
    * Update heat file formatting/guideline and updating testbed information
    * Fix Openstack experiment update part
    * Resolve issue in creation of deter experiment
    * Update Json string format to show Text area of experiment
    
    Co-authored-by: ThazinH <thazin15@nus.edu.sg>

---
+++ src/main/java/sg/ncl/testbed_interface/OpenStackCreateForm.java
@@ -0,0 +1,41 @@
+package sg.ncl.testbed_interface;
+
+import javax.validation.constraints.Size;
+
+public class OpenStackCreateForm {
+    @Size(min=1, message="Invalid email/password.")
+    private String loginPassword;
+
+    private String errMsg = null;
+
+    private int isActivate = 0;
+
+    public OpenStackCreateForm() {
+
+    }
+
+    public void setLoginPassword(String password) {
+        this.loginPassword = password;
+    }
+
+    public String getLoginPassword() {
+        return loginPassword;
+    }
+
+    public void setErrMsg(String error) {
+        this.errMsg = error;
+    }
+
+    public String getErrMsg() {
+        return errMsg;
+    }
+
+    public void setIsActivate(int isActivate) {
+        this.isActivate = isActivate;
+    }
+
+    public int getIsActivate() {
+        return this.isActivate;
+    }
+
+}
