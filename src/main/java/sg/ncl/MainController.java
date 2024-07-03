0c3c93026676a8d06612b1dd4b5653786cb0a5d1
ThazinH <96106381+ThazinH@users.noreply.github.com>
2024-07-03 14:16:26 +0800

    Update MainController.java

--- src/main/java/sg/ncl/MainController.java
+++ src/main/java/sg/ncl/MainController.java
@@ -60,7 +60,7 @@ public class MainController {
     private static final String OS_TOKEN = "OS_Token";
     private static final String SESSION_LOGGED_IN_USER_ID = "loggedInUserId";
 
-    private static final String CONTACT_EMAIL = "support@ncl.sg";
+    private static final String CONTACT_EMAIL = "contact_email";
     private static final String CONTACT_US_FORM = "contactUsForm";
     private static final String RECAPTCHA_SECRET_KEY = "secret_key";
     private static final String BookNode_FORM = "BookNodeForm";
